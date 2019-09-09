7be86883f734cc581ae72c9010e25e74dd699af3
Ross Frischmuth <frischmuth@gmail.com>
2019-09-09 16:17:24 -0700

    Collect data, pose questions, more EDA

--- data_prep.py
+++ data_prep.py
@@ -8,5 +8,7 @@ def read_data_remove_zeros(csv):
     data = pd.read_csv(csv)
     data = data[data['Reported'] == 'Y'].copy()
     data.set_index('School_Name', inplace=True)
+    #data.rename(columns={data.columns[1]: 'Enrollment'}, inplace=True)
+    # data = data[data['Enrollment'] > 0].copy()
     data.sort_index()
     return(data)
