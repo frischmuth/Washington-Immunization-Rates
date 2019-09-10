d8075ec49dc964b8bb4454346ae78e8a40b18ed7
Ross Frischmuth <frischmuth@gmail.com>
2019-09-10 15:43:34 -0700

    update function to cleanly import school data

--- data_prep.py
+++ data_prep.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 
 
-def read_data_remove_zeros(csv):
+def read_vaccine_data(csv):
     data = pd.read_csv(csv)
     data = data[data['Reported'] == 'Y'].copy()
-    # data.set_index('School_Name', inplace=True)
-    #data.rename(columns={data.columns[1]: 'Enrollment'}, inplace=True)
-    # data = data[data['Enrollment'] > 0].copy()
+    data['School_District'] = data['School_District'].str.upper()
+    data['School_Name'] = data['School_Name'].str.upper()
+    data.set_index(['School_District', 'School_Name'], inplace=True)
+    data.rename(columns={data.filter(like='enroll').columns[0]: 'Enrollment'}, inplace=True)
+    data = data[data['Enrollment'] > 0].copy()
     data.sort_index()
     return(data)
