45cd528d2cc34458d1a085cee7f81989933c2dd7
Ross Frischmuth <frischmuth@gmail.com>
2019-09-10 11:28:47 -0700

    Merge race and vaccine tables, first scatter

--- data_prep.py
+++ data_prep.py
@@ -7,7 +7,7 @@ import scipy.stats as stats
 def read_data_remove_zeros(csv):
     data = pd.read_csv(csv)
     data = data[data['Reported'] == 'Y'].copy()
-    data.set_index('School_Name', inplace=True)
+    # data.set_index('School_Name', inplace=True)
     #data.rename(columns={data.columns[1]: 'Enrollment'}, inplace=True)
     # data = data[data['Enrollment'] > 0].copy()
     data.sort_index()
